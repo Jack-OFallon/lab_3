e341f42dea52e744d0a0c7b40184738c590b96a2
funkydmonky <joof6259@colorado.edu>
2020-02-11 16:11:41 -0700

    Adding all of the files for lab 3

--- lab3_script.sh
+++ lab3_script.sh
@@ -10,5 +10,12 @@ read exp
 grep $exp $file_name
 egrep -c "[0-9]{3}\-[0-9]{3}\-[0-9]{4}" "regex_practice.txt"
 grep -c "^.*@.*com" "regex_practice.txt"
+egrep -o "303\-[0-9]{3}\-[0-9]{4}" "regex_practice.txt"
+egrep -o "geocities.com" "regex_practice.txt" >> "email_results.txt"
+
+git "add" "email_results.txt"
+git commit -m "Here are your email results:"
 
 #Make sure to document how you are solving each problem!
+#We solved these problems by following the steps given in lecture, -c searches for characters, of which we could seach all numbers 0-9 and have them divided up 3 / 3 / 4 because that is the format of any phone number.
+#The email problems were just as simple, following correct syntax to search for the characters used in the format of a email address.
